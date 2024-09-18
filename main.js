65c058f68236ebb29b6bddebcc24d1a205e3fa51
Bencze85 <84965056+Bencze85@users.noreply.github.com>
2024-09-18 14:47:33 +0200

    Hozzáadtam a HTML-t

---
+++ main.js
@@ -0,0 +1,11 @@
+/*document.getElementById("openmenu").onclick() 
+{
+    document.getElementById("navigáció").classList.toggle("nyitva");
+}
+*/
+
+
+function openmenu ()
+{
+    document.getElementById ("nav").classList.toggle("open");
+}
